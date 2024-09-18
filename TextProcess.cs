8b16c131b992c34b0ad3ce13358eea4da1579e96
Koorineroki <Koorineroki@outlook.com>
2024-09-18 14:06:05 +0800

    添加项目文件。

---
+++ TextProcess.cs
@@ -0,0 +1,30 @@
+﻿using JiebaNet.Analyser;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PicKeyFinder
+{
+    internal class TextProcess
+    {
+        // Output dictionary of keywords and their weights.
+        Dictionary<string, double> wordWeights = new Dictionary<string, double>();
+        // Allowed parts of speech (nouns only)
+        IEnumerable<string> allowedPos = new List<string> { "n" };
+
+        // 提取名词并标记权重
+        public Dictionary<string, double> GetKeywords(string inputText)
+        {
+            Reset();
+            var extractor = new TfidfExtractor();
+            var keywords = extractor.ExtractTagsWithWeight(inputText, 5, this.allowedPos);
+
+            foreach (var keyword in keywords) this.wordWeights.Add(keyword.Word, keyword.Weight);
+            return this.wordWeights;
+        }
+
+        void Reset()
+        {
+            wordWeights.Clear();
+        }
+    }
+}
